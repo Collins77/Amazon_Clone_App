51f79a175ef9847cc982f946a0c909f4de42a25f
Collins77 <collinsmuemah@gmail.com>
2023-05-26 13:20:40 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/opasso_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.opasso_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/global_variables.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GlobalVariables {
+  // COLORS
+  static const appBarGradient = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 27,77,62),
+      Color.fromARGB(255, 0, 106, 78),
+    ],
+    stops: [0.5, 1.0],
+  );
+
+  static const secondaryColor = Color.fromARGB(255, 27, 77, 62);
+  static const backgroundColor = Colors.white;
+  static const Color greyBackgroundCOlor = Color(0xffebecee);
+  static var selectedNavBarColor = Colors.cyan[800]!;
+  static const unselectedNavBarColor = Colors.black87;
+}
