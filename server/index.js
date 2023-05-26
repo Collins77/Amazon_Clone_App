62f3c643a7db3ca839d714351c582a5bb31e9ff8
Collins77 <collinsmuemah@gmail.com>
2023-05-26 16:30:20 +0300

    updates

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const authRouter = require('.routes/auth');
+const authRouter = require('./routes/auth');
 
-
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
+const DB = 'mongodb+srv://Collins77:Cmmuemah%4077@cluster0.biccstt.mongodb.net/?retryWrites=true&w=majority'
 
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-})
+// middleware
+app.use(express.json());
+app.use(authRouter);
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Database connection established");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
