67d956b0a6b03a2947232002d8f0e9e616c64cc6
Collins77 <collinsmuemah@gmail.com>
2023-05-29 15:05:19 +0300

    updates

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -4,8 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:opasso_app/constants/error_handling.dart';
 import 'package:opasso_app/constants/global_variables.dart';
 import 'package:opasso_app/constants/utils.dart';
+import 'package:opasso_app/features/home/screens/home_screen.dart';
 import 'package:opasso_app/models/user.dart';
 import 'package:http/http.dart' as http;
+import 'package:opasso_app/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   void signUpUser({
@@ -24,11 +28,13 @@ class AuthService {
           type: '',
           token: '');
 
-      http.Response res = await http.post(Uri.parse('$uri/api/signup'),
-          body: user.toJson(),
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=utf-8',
-          });
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signup'),
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+      );
       httpErrorHandle(
         response: res,
         context: context,
@@ -55,12 +61,53 @@ class AuthService {
           headers: <String, String>{
             'Content-Type': 'application/json; charset=utf-8',
           });
-      print(res.body);
       httpErrorHandle(
         response: res,
         context: context,
-        onSuccess: () {},
+        onSuccess: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+          Navigator.pushNamedAndRemoveUntil(
+              context, HomeScreen.routeName, (route) => false);
+        },
       );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+// get user data
+  void getUserData(
+    BuildContext context,
+  ) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('x-auth-token');
+
+      if (token == null) {
+        prefs.setString('x-auth-token', '');
+      }
+
+      
+      // http.Response res = await http.post(Uri.parse('$uri/api/signin'),
+      //     body: jsonEncode({
+      //       'email': email,
+      //       'password': password,
+      //     }),
+      //     headers: <String, String>{
+      //       'Content-Type': 'application/json; charset=utf-8',
+      //     });
+      // httpErrorHandle(
+      //   response: res,
+      //   context: context,
+      //   onSuccess: () async {
+      //     SharedPreferences prefs = await SharedPreferences.getInstance();
+      //     Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+      //     await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+      //     Navigator.pushNamedAndRemoveUntil(context, HomeScreen.routeName, (route) => false);
+      //   },
+      // );
     } catch (e) {
       showSnackBar(context, e.toString());
     }
